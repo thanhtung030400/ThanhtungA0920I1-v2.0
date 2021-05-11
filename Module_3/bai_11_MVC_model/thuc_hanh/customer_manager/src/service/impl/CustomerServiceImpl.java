1218807e45ec72e025e5079ef01886f5f60a12da
thanhtung030400 <thanhtung030400@gmail.com>
2021-05-11 13:53:13 +0700

    update

---
+++ Module_3/bai_11_MVC_model/thuc_hanh/customer_manager/src/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,48 @@
+package service.impl;
+
+import model.Customer;
+import service.CustomerService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CustomerServiceImpl  implements CustomerService {
+
+    private  static Map<Integer, Customer> customerMap;
+
+    static {
+        customerMap = new HashMap<>();
+        customerMap.put(1, new Customer(1, "John", "john@codegym.vn", "Hanoi"));
+        customerMap.put(2, new Customer(2, "Bill", "bill@codegym.vn", "Danang"));
+        customerMap.put(3, new Customer(3, "Alex", "alex@codegym.vn", "Saigon"));
+        customerMap.put(4, new Customer(4, "Adam", "adam@codegym.vn", "Beijin"));
+        customerMap.put(5, new Customer(5, "Sophia", "sophia@codegym.vn", "Miami"));
+        customerMap.put(6, new Customer(6, "Rose", "rose@codegym.vn", "Newyork"));
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(Customer customer) {
+
+    }
+
+    @Override
+    public Customer findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, Customer customer) {
+
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+}
